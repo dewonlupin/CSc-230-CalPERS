996100f66c432985604b99873ef2b9742c9d61db
JahandadP <66800012+JahandadP@users.noreply.github.com>
2020-06-11 14:28:48 -0700

    File added

---
+++ activation.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 1000px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+      <p>
+        An email has been sent to your Email for verification.
+      </p>
+
+    </div>
+</body>
+</html>
