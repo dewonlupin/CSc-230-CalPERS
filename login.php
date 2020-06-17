c0877bdcb0ea766e24e6fd0f6403d32f6ed09055
Aanal Gandhi <analgandhi@csus.edu>
2020-06-16 18:16:52 -0700

    Template integration,Login, Register,Reset Pwd

--- login.php
+++ login.php
@@ -90,34 +90,74 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Required meta tags-->
     <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
+    <title><img src="images/Calpers-logo.png" />Au Register Forms by Colorlib</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $email; ?>">
-                <span class="help-block"><?php echo $email_err; ?></span>
+    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+<img src="images/calpersLogo.jpg" style="height:100px;widht:150px;margin:-20px;margin-left:0px;" />
+                    <h2 class="title">Sign In</h2>
+                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">User Name</label>
+                                    <input class="input--style-4" type="text"  name="username"  value="<?php echo $email; ?>">
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="row">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Password</label>
+                                    <input class="input--style-4"  type="password" name="password">
+                                    <span class="help-block"><?php echo $password_err; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-t-15">
+                      		<input type="submit" class="btn btn--radius-2 btn--blue" value="Log In">
+                        </div>
+			<div class="row"><p>Don't have an account? <a href="register.php">Sign up here</a>.</p></div>
+                    </form>
+                </div>
             </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Log In">
-            </div>
-            <p>Don't have an account? <a href="register.php">Sign up here</a>.</p>
-        </form>
+        </div>
     </div>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+
 </body>
 </html>
--- register.php
+++ register.php
@@ -2,7 +2,8 @@
 // Include config file
 require_once "config.php";
 // Define variables and initialize with empty values
-$firstname = $lastname = $email = $role = $password = "";
+$firstname = $lastname = $email = $password = "";
+$role = "test";
 $confirm_password = $employee = $calpers_id = $company =  "";
 $firstname_err = $lastname_err = $email_err = $role_err = $password_err = "";
 $confirm_password_err = $employee_err = $calpers_id_err = $company_err = "";
@@ -42,76 +43,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    // validate firstname
-    if(empty(trim($_POST["firstname"]))){
-        $firstname_err = "Please enter a first name.";
-    }
-    else{
-        $firstname = trim($_POST["firstname"]);
-    }
-    // validate lastname
-    if(empty(trim($_POST["lastname"]))){
-        $lastname_err = "Please enter a last name.";
-    }
-    else{
-        $lastname = trim($_POST["lastname"]);
-    }
-    //validate employee status
-    if (!isset($_POST["employee"])){
-      $employee_err = "Please select one.";
-    }
-    else{
-        $employee = trim($_POST["employee"]);
-    }
-    // validate role
-    if(empty(trim($_POST["role"]))){
-        $role_err = "Please enter a role.";
-    }
-    else{
-        $role = trim($_POST["role"]);
-    }
-    //validate CalPERSID
-    if(empty(trim($_POST["calpers_id"]))){
-        $calpers_id_err = "Please enter your CalPERS ID.";
-    }
-    else{
-        $calpers_id = trim($_POST["calpers_id"]);
-    }
-
-    // validate company
-    if(empty(trim($_POST["company"]))){
-        //echo "pirayesh";
-        $company_err = "Please enter the name of company.";
-    }
-    else{
-        $company = trim($_POST["company"]);
-    }
-    if ($employee=="yes"){
-      $company_err=$company="";
-
-    }elseif ($employee=="no") {
-      $calpers_id_err=$calpers_id="";
-      $role_err=$role="";
-    }
-
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
+   
     // Check input errors before inserting in database
     if( empty($firstname_err) && empty($lastname_err)&& empty($employee_err) &&
     empty($role_err) && empty($calpers_id_err)&& empty($company_err)&&
@@ -163,92 +95,167 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 ?>
 <script type="text/javascript">
-function is_calpers_employee() {
-    if (document.getElementById('employee_true').checked) {
-        document.getElementById('calpers_block').style.display = 'block';
-        document.getElementById('outsider_block').style.display = 'none';
-    }
-    else if (document.getElementById('employee_false').checked) {
-    document.getElementById('calpers_block').style.display = 'none';
-    document.getElementById('outsider_block').style.display = 'block';
-  }
+    function is_calpers_employee() {
+        if (document.getElementById('employee_true').checked) {
+            document.getElementById('calpers_block').style.display = 'block';
+            document.getElementById('outsider_block').style.display = 'none';
+        }
+        else if (document.getElementById('employee_false').checked) {
+            document.getElementById('calpers_block').style.display = 'none';
+            document.getElementById('outsider_block').style.display = 'block';
+        }
 
-}
+    }
 </script>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill the form below to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($firstname_err)) ? 'has-error' : ''; ?>">
-                <label>First Name</label>
-                <input type="text" name="firstname" class="form-control" value="<?php echo $firstname; ?>">
-                <span class="help-block"><?php echo $firstname_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($lastname_err)) ? 'has-error' : ''; ?>">
-                <label>Last Name</label>
-                <input type="text" name="lastname" class="form-control" value="<?php echo $lastname; ?>">
-                <span class="help-block"><?php echo $lastname_err; ?></span>
-            </div>
-            <div  class="form-group <?php echo (!empty($employee_err)) ? 'has-error' : ''; ?>">
-                <label>Are you currently employed by CalPERS?</label><br>
-                <span><input type="radio" onchange="javascript:is_calpers_employee();" name="employee" id="employee_true"   value="yes" <?php if (isset($_POST['employee']) && $_POST['employee']=="yes") echo "checked";?>   > Yes </span>
-                <span><input type="radio" onchange="javascript:is_calpers_employee();" name="employee" id="employee_false"  value="no"  <?php if (isset($_POST['employee']) && $_POST['employee']=="no") echo "checked";?> > No</span><br>
-                <span class="help-block"><?php echo $employee_err; ?></span>
-            </div>
-            <div id="calpers_block" style="display:none">
-              <div class="form-group <?php echo (!empty($role_err)) ? 'has-error' : ''; ?>">
-                  <label>Role</label>
-                  <input type="text" name="role" class="form-control" value="<?php echo $role; ?>">
-                  <span class="help-block"><?php echo $role_err; ?></span>
-              </div>
-              <div class="form-group <?php echo (!empty($calpers_id_err)) ? 'has-error' : ''; ?>">
-                  <label>CalPERS ID# </label>
-                  <input type="text" name="calpers_id" class="form-control" value="<?php echo $calpers_id; ?>">
-                  <span class="help-block"><?php echo $calpers_id_err; ?></span>
-              </div>
-            </div>
-            <div id="outsider_block" style="display:none">
-              <div class="form-group <?php echo (!empty($company_err)) ? 'has-error' : ''; ?>">
-                  <label>Company</label>
-                  <input type="text" name="company" class="form-control" value="<?php echo $company; ?>">
-                  <span class="help-block"><?php echo $company_err; ?></span>
-              </div>
-            </div>
-            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-                <label>Email Address</label>
-                <input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
-                <span class="help-block"><?php echo $email_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                <span class="help-block"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Sign Up">
-                <input type="reset" class="btn btn-default" value="Reset">
-            </div>
-            <p>Already have an account? <a href="login.php">Log in here</a>.</p>
-        </form>
-    </div>
-    <script type="text/javascript">is_calpers_employee();</script>
 
-</body>
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
+    <title>Au Register Forms by Colorlib</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+			<img src="images/calpersLogo.jpg" style="height:100px;widht:150px;margin:-20px;margin-left:0px;" />
+                    <h2 class="title">Sign Up</h2>
+                    <p>Please fill the form below to create an account.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF"]); ?>
+                        " method="post">
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group form-group <?php echo (!empty($firstname_err)) ? 'has-error' : ''; ?>">
+                                    <label class="label">first name</label>
+                                    <input type="text" name="firstname" class="input--style-4" value="<?php echo $firstname; ?>">
+                                    <span class="help-block"><?php echo $firstname_err; ?></span>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group form-group <?php echo (!empty($lastname_err)) ? 'has-error' : ''; ?>"
+                                    <label>Last Name</label>
+                                    <input type="text" name="lastname" class="input--style-4" value="<?php echo $lastname; ?>">
+                                    <span class="help-block"><?php echo $lastname_err; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    Are you currently employed by CalPERS?
+
+                                    <label class="radio-container">
+                                        Yes
+                                        <input type="radio" name="gender" onchange="javascript:is_calpers_employee();" name="employee" id="employee_true" value="yes" <?php if (isset($_POST['employee']) && $_POST['employee']=="yes") echo "checked";?>   >
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <label class="radio-container">
+                                        No
+                                        <input type="radio" name="gender" onchange="javascript:is_calpers_employee();" name="employee" id="employee_false" value="no" <?php if (isset($_POST['employee']) && $_POST['employee']=="no") echo "checked";?>   >
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <!--<label>Are you currently employed by CalPERS?</label><br>
+                                    <span><input type="radio" onchange="javascript:is_calpers_employee();" name="employee" id="employee_true" value="yes" <?php if (isset($_POST['employee']) && $_POST['employee']=="yes") echo "checked";?>   > Yes </span>
+                                    <span><input type="radio" onchange="javascript:is_calpers_employee();" name="employee" id="employee_false" value="no"  <?php if (isset($_POST['employee']) && $_POST['employee']=="no") echo "checked";?> > No</span><br>-->
+                                    <!--   <span class="help-block"><?php echo $employee_err; ?></span>-->
+                                </div>
+                            </div>
+                            <div class="col-2">
+
+                                <div id="calpers_block" style="display:none">
+                                    
+                                    <div class="input-group <?php echo (!empty($calpers_id_err)) ? 'has-error' : ''; ?>">
+                                        <label>CalPERS ID# </label>
+                                        <input type="text" name="calpers_id" class="input--style-4" value="<?php echo $calpers_id; ?>">
+                                        <span class="help-block"><?php echo $calpers_id_err; ?></span>
+                                    </div>
+                                </div>
+                                <div id="outsider_block" style="display:none">
+                                    <div class="input-group <?php echo (!empty($company_err)) ? 'has-error' : ''; ?>">
+                                        <label>Company</label>
+                                        <input type="text" name="company" class="input--style-4" value="<?php echo $company; ?>">
+                                        <span class="help-block"><?php echo $company_err; ?></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                                    <label class="label">Email</label>
+                                    <input type="text" name="email" class="input--style-4" value="<?php echo $email; ?>">
+                                    <span class="help-block"><?php echo $email_err; ?></span>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-2">
+                                <div class="input-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                                    <label class="label">Password</label>
+                                    <input type="password" name="password" class="input--style-4" value="<?php echo $password; ?>">
+                                    <span class="help-block"><?php echo $password_err; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2">
+                                <div class="input-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                                    <label class="label">Confirm Password</label>
+                                    <input type="password" name="confirm_password" class="input--style-4" value="<?php echo $confirm_password; ?>">
+                                    <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="p-t-15" style="text-align:center;">
+
+                            <input type="submit" style="width:30%" class="btn btn--radius-2 btn--blue" value="Sign Up">
+                            <input type="reset" style="width:30%" class="btn btn--radius-2 btn--blue" value="Reset">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
 </html>
+<!-- end document-->
